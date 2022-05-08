ad32071d02b4cac219645d92b144af2bdcb24622
ANDREEA GEORGIANA CREITA <andreea.creita@s.unibuc.ro>
2022-05-09 01:24:10 +0300

    DataBase set & Pages Created

---
+++ ProiectMDS/Models/Page.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectMDS.Models
+{
+    public class Page
+    {
+        public int Id { get; set; } //va fi automat recunoscut ca primary key
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Slug { get; set; }
+        [Required]
+        public string Content { get; set; }
+
+        public int Sorting { get; set; }
+
+    }
+}
--- ProiectMDS/Startup.cs
+++ ProiectMDS/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProiectMDS.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,7 +26,12 @@ namespace ProiectMDS
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            //register services and dependencies in the configureService method
+            services.AddDbContext<ProiectMDSContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ProiectMDSContext")));
         }
+
+       
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
