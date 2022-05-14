979c74e26e0619287549ada188218ddab9e37ef3
Ioana <Ioana@DESKTOP-LRNIC1J>
2022-05-14 12:28:28 +0300

    Add project files.

---
+++ ProiectMDS/Models/Page.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectMDS.Models
+{
+    public class Page
+    {
+        public int Id { get; set; } //va fi automat recunoscut ca primary key <3
+        [Required, MinLength(2, ErrorMessage = "Minimum length is 2")]
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        [Required, MinLength(4, ErrorMessage = "Minimum length is 4")]
+        public string Content { get; set; }
+        public int Sorting { get; set; }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# ProiectMDS
+
+***Trello Link***
+*https://trello.com/b/6HfseaVx/mds-20*
